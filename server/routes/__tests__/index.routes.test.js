6236d6d7c316695995048c917ffb6238917e8cbc
Nate Walters <newsproutsmedia@gmail.com>
2021-06-27 01:00:02 -0700

    Update root route test

--- server/routes/__tests__/index.routes.test.js
+++ server/routes/__tests__/index.routes.test.js
@@ -2,15 +2,18 @@ const request = require('supertest');
 const app = require('../../app');
 
 describe('api test /test', () => {
-  it('should respond with 200 status to GET method', async () => {
-    const response = await request(app)
-      .get('/test');
-    expect(response.statusCode).toBe(200);
+  it('should respond with 200 status to GET method', (done) => {
+    request(app)
+      .get('/test')
+      .expect(200)
+      .end(done);
   });
 
-  it('should respond with 404 status to invalid route', async () => {
-    const response = await request(app)
-      .get('/invalid');
-    expect(response.statusCode).toBe(404);
+  it('should respond with 200 status to any route and serve HTML', (done) => {
+    request(app)
+      .get('/')
+      .expect('Content-Type', 'text/html; charset=UTF-8')
+      .expect(200)
+      .end(done);
   });
 });
--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -1,15 +1,14 @@
+const path = require('path');
 const logger = require('../logging/logger');
 
 module.exports = (app) => {
-  // a test route to make sure we can reach the backend
-  // this would normally go in a routes file
   app.get('/test', (req, res) => {
     logger.debug('This is the "/test" route.');
     res.status(200).send('Welcome to the backend!');
   });
 
-  // All other GET requests not handled before will return our React app
-  app.get('*', (req, res) => {
-    res.status(404).send('Not Found');
+  // All other GET requests not handled before will return the React app
+  app.get('/', (req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname, '../../client/build', 'index.html'));
   });
 };
