c848a4af71a387b032a3d6b72172fd188faaf9e2
Nate Walters <newsproutsmedia@gmail.com>
2021-06-27 20:16:28 -0700

    Add App.js layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,64 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import theme from './theme';
+import NavBar from './components/navigation/NavBar';
+import Introduction from './components/text/Introduction';
+import DataSection from './components/data/DataSection';
+import SignupSection from './components/signup/SignupSection';
+import Footer from './components/navigation/Footer';
 
 function App() {
-  const [welcome, setWelcome] = useState();
-  const [post, setPost] = useState();
-  const [responseToPost, setResponseToPost] = useState();
-
-  const getBackend = async (url) => {
-    await axios.get(url)
-      .then((response) => {
-        setWelcome(response.data);
-      });
+  const appStyle = {
+    backgroundColor: theme.white,
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: theme.grey,
   };
-
-  const postToBackend = async (e) => {
-    e.preventDefault();
-    const postMessage = { message: post };
-    await axios.post('/api/message', postMessage)
-      .then((response) => setResponseToPost(response.data.message))
-      .catch((error) => {
-        setResponseToPost(error.message);
-      });
-  };
-
-  const handleInputChange = (e) => {
-    setPost(e.target.value);
-  };
-
-  useEffect(() => {
-    getBackend('/api/test/');
-  }, [welcome]);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcome To React</h1>
-        <p>
-          Edit
-          <code> src/App.js </code>
-          and save to reload.
-        </p>
-        <p>{welcome}</p>
-        <form onSubmit={postToBackend}>
-          <label htmlFor="postInput">
-            Send POST Request To Server:
-            <input
-              id="postInput"
-              type="text"
-              value={post}
-              onChange={handleInputChange}
-            />
-          </label>
-          <button type="submit">SEND</button>
-        </form>
-        <p>{responseToPost}</p>
-      </header>
-
+    <div id="app" data-testid="app" style={appStyle}>
+      <NavBar/>
+      <Introduction/>
+      <DataSection/>
+      <SignupSection/>
+      <Footer/>
     </div>
   );
 }
